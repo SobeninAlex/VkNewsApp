96210df262f9d87e2d526f757965a2f0ad59cd5c
Алексей Собенин <saa@renue.ru>
2024-03-12 14:32:04 +0500

    change git ignore

--- app/src/main/java/com/example/vknewsapp/MainActivity.kt
+++ app/src/main/java/com/example/vknewsapp/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,20 +20,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewScreen()
+            PreviewPostCard()
         }
     }
 }
 
 @Composable
-private fun PreviewFunctions() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background)
-            .padding(8.dp),
-    ) {
-        PostCard()
+private fun PreviewPostCard() {
+    VkNewsAppTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(8.dp),
+        ) {
+            PostCard()
+        }
     }
 }
 
