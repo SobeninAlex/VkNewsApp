ff790dc5fbfe0333026aea26e55dc97ccf1f4c76
Алексей Собенин <saa@renue.ru>
2024-03-14 21:10:10 +0500

    add navigation with jetpack compose nav

--- app/src/main/java/com/example/vknewsapp/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsapp/ui/VkNewsMainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.vknewsapp.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -10,13 +11,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.vknewsapp.MainViewModel
 import com.example.vknewsapp.navigation.AppNavGraph
+import com.example.vknewsapp.navigation.Screen
 
 @Composable
 fun MainScreen(mainViewModel: MainViewModel) {
@@ -44,7 +51,13 @@ fun MainScreen(mainViewModel: MainViewModel) {
                     NavigationBarItem(
                         selected = currentRoute == navItem.screen.route,
                         onClick = {
-                            navHostController.navigate(navItem.screen.route)
+                            navHostController.navigate(navItem.screen.route) {
+                                popUpTo(Screen.Home.route) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
                         },
                         icon = {
                             Icon(navItem.icon, contentDescription = null)
@@ -71,12 +84,26 @@ fun MainScreen(mainViewModel: MainViewModel) {
                 HomeScreen(mainViewModel = mainViewModel, paddingValues = paddingValues)
             },
             favouriteScreenContent = {
-                Text(text = "Favourite", color = MaterialTheme.colorScheme.onPrimary)
+                TextCounter(name = "Favourite")
             },
             profileScreenContent = {
-                Text(text = "Profile", color = MaterialTheme.colorScheme.onPrimary)
+                TextCounter(name = "Profile")
             }
         )
     }
 
+}
+
+@Composable
+private fun TextCounter(name: String) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Text(
+        modifier = Modifier
+            .clickable { count++ },
+        text = "$name Count: $count",
+        color = MaterialTheme.colorScheme.onPrimary
+    )
 }
